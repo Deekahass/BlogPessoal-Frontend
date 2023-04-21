4e0af66ba5d1665ee2bec3e2dfaba9129db8494d
Andreza Silvestre <deza.silvestre@hotmail.com>
2023-04-21 20:22:06 -0300

    footer e navbar

---
+++ src/components/estaticos/footer/footer.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Typography, Grid } from '@material-ui/core'
+import { Box } from '@mui/material'
+import InstagramIcon from '@material-ui/icons/Instagram'
+import GithubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from '@material-ui/icons/LinkedIn'
+import './Footer.css'
+
+
+function Footer() {
+  return (
+    <>
+        <Grid container direction="row" justifyContent="center" alignItems="center" color="primary">
+            <Grid alignItems="center" item xs={12}>
+                <Box className='footer-box1'>
+                    <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                        <Typography variant="h5" align="center" gutterBottom className='footer-textos'>
+                            Minhas Redes:
+                        </Typography>
+                    </Box>
+                    <Box display="flex" alignItems="center" justifyContent="center">
+                        <a href="https://github.com/Deekahass" target="_blank">
+                            <GithubIcon className='redes-sociais github' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/andreza-silvestre/" target="_blank">
+                            <LinkedInIcon className='redes-sociais linkedin' />
+                        </a>
+                    </Box>
+                </Box>
+                <Box className='footer-box2'>
+                    <Box paddingTop={1}>
+                        <Typography variant="subtitle2" align="center" gutterBottom className='footer-textos' >
+                            © 2023 Copyright:
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <a target="_blank" href="https://brasil.generation.org" className='footer-link'>
+                            <Typography variant="subtitle2" gutterBottom className='footer-textos' align="center">
+                                brasil.generation.org
+                            </Typography>
+                        </a>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    </>
+  )
+}
+
+export default Footer
